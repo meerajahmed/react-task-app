a6a22438bc5f311756827f8309ba62324985ff2a
Meeraj Ahmed <meerajahmed@gmail.com>
2018-08-08 03:04:40 +0530

    task app using react

--- src/components/task-app.js
+++ src/components/task-app.js
@@ -1,14 +1,41 @@
-import React from 'react';
+import React, {Component} from 'react';
 import AddTask from "./add-task";
-import TaskList from "./task-list";
+import TaskListView from "./task-list-view";
 
-const TaskApp = () => {
-  return (
-    <div>
-      <AddTask />
-      <TaskList />
-    </div>
-  );
-};
+export default class TaskApp extends Component {
 
-export default TaskApp;
+  state = {
+    tasks: []
+  };
+
+  handleSaveTask = (task) => {
+    this.setState((prevState) => ({
+      tasks: [...prevState.tasks, task]
+    }));
+  };
+
+  handleStatusChange = (id, status) => {
+    this.setState((prevState) => {
+      let tasks = prevState.tasks.map((task) => {
+        if (id === task.id) {
+          return {
+            ...task,
+            status
+          }
+        }
+        return task;
+      });
+      return {tasks}
+    });
+  };
+
+  render() {
+    return (
+      <div className="container my-5">
+        <AddTask handleSaveTask={this.handleSaveTask}/>
+        { this.state.tasks.length > 0 &&
+        <TaskListView tasks={this.state.tasks} handleStatusChange={this.handleStatusChange}/> }
+      </div>
+    );
+  }
+}
