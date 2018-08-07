b0f51277718280ca5e5ff206358baca66fc39490
Meeraj Ahmed <meerajahmed@gmail.com>
2018-08-08 03:04:57 +0530

    task app using react

--- src/index.js
+++ src/index.js
@@ -2,8 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import TaskApp from "./components/task-app";
 
-const App = () => <h1>React Seed App</h1>;
-
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<TaskApp />, document.getElementById('root'));
 
