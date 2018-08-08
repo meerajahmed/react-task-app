dde325842eb48c929b79e43ef857b86108bbf882
Meeraj Ahmed <meerajahmed@gmail.com>
2018-08-09 01:34:15 +0530

    managing state using redux

--- src/store/index.js
+++ src/store/index.js
@@ -1,3 +1,6 @@
-/**
- * Created by meeahmed on 8/9/2018.
- */
+import {createStore} from 'redux';
+import {devToolsEnhancer} from 'redux-devtools-extension';
+
+import reducer from '../reducers/tasks';
+
+export default createStore(reducer, devToolsEnhancer());
