387b928471e646cdcdfb91d35e8778a2c04f8370
Meeraj Ahmed <meerajahmed@gmail.com>
2018-08-10 01:34:56 +0530

    managing state using redux

--- src/actions/updateTaskStatus.js
+++ src/actions/updateTaskStatus.js
@@ -1,3 +1,6 @@
-/**
- * Created by meeahmed on 8/10/2018.
- */
+export const UPDATE_TASK_STATUS = 'UPDATE_TASK_STATUS';
+
+export default (id, status) => ({
+  type: UPDATE_TASK_STATUS,
+  payload: {id, status}
+});
--- src/reducers/tasks.js
+++ src/reducers/tasks.js
@@ -1,3 +1,29 @@
-/**
- * Created by meeahmed on 8/9/2018.
- */
+import {ADD_TASK} from '../actions/addTask';
+import {UPDATE_TASK_STATUS} from '../actions/updateTaskStatus';
+
+const defaultState = [];
+
+export default (state = defaultState, {type, payload}) => {
+  switch (type) {
+
+    case ADD_TASK:
+      const {task} = payload;
+      return [...state, task];
+
+    case UPDATE_TASK_STATUS:
+      const {id, status} = payload;
+      return state.map((task) => {
+        if (id === task.id) {
+          return {
+            ...task,
+            status
+          }
+        }
+        return task;
+      });
+
+    default:
+      return state;
+
+  }
+}
