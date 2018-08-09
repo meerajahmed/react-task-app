e9bd3a00a045d460181b20b7d6ed9093eff2af04
Meeraj Ahmed <meerajahmed@gmail.com>
2018-08-10 01:34:38 +0530

    managing state using redux

--- src/components/task-app.js
+++ src/components/task-app.js
@@ -2,40 +2,12 @@ import React, {Component} from 'react';
 import AddTask from "./add-task";
 import TaskListView from "./task-list-view";
 
-export default class TaskApp extends Component {
+const TaskApp = (props) => (
+  <div className="container my-5">
+    <AddTask handleAddTask={props.handleAddTask}/>
+    { props.tasks.length > 0 &&
+    <TaskListView tasks={props.tasks} handleStatusChange={props.handleStatusChange}/> }
+  </div>
+);
 
-  state = {
-    tasks: []
-  };
-
-  handleSaveTask = (task) => {
-    this.setState((prevState) => ({
-      tasks: [...prevState.tasks, task]
-    }));
-  };
-
-  handleStatusChange = (id, status) => {
-    this.setState((prevState) => {
-      let tasks = prevState.tasks.map((task) => {
-        if (id === task.id) {
-          return {
-            ...task,
-            status
-          }
-        }
-        return task;
-      });
-      return {tasks}
-    });
-  };
-
-  render() {
-    return (
-      <div className="container my-5">
-        <AddTask handleSaveTask={this.handleSaveTask}/>
-        { this.state.tasks.length > 0 &&
-        <TaskListView tasks={this.state.tasks} handleStatusChange={this.handleStatusChange}/> }
-      </div>
-    );
-  }
-}
+export default TaskApp;
--- src/containers/task-app-container.js
+++ src/containers/task-app-container.js
@@ -1,3 +1,14 @@
-/**
- * Created by meeahmed on 8/9/2018.
- */
+import {connect} from "react-redux";
+
+import TaskApp from "../components/task-app";
+import addTask from "../actions/addTask";
+import updateTaskStatus from "../actions/updateTaskStatus";
+
+const mapStateToProps = (state) => ({tasks: state});
+
+const mapDispatchToProps = (dispatch) => ({
+  handleAddTask: (task) => dispatch(addTask(task)),
+  handleStatusChange: (id, status) => dispatch(updateTaskStatus(id, status))
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(TaskApp);
