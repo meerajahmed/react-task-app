b12ff807faccc99bc8f76e957d328c0a0aaddf90
Meeraj Ahmed <meerajahmed@gmail.com>
2018-08-15 22:24:45 +0530

    buffer incoming messages using channel

--- src/sagas/addTaskAsync.js
+++ src/sagas/addTaskAsync.js
@@ -1,11 +1,13 @@
-import {take, put} from 'redux-saga/effects';
+import {take, put, call, actionChannel} from 'redux-saga/effects';
 import {ADD_TASK_ASYNC} from "../actions/addTaskAsync";
 import {delay} from "redux-saga";
 import addTask from "../actions/addTask";
 
 export default function* addTaskAsync() {
+  // buffer incoming ADD_TASK_ASYNC messages if the Saga is not yet ready to take them
+  const addTaskChannel = yield actionChannel(ADD_TASK_ASYNC);
   while (true) {
-    const {payload} = yield take(ADD_TASK_ASYNC);
+    const {payload} = yield take(addTaskChannel);
     yield delay(1000); // save in DB here...
     yield put(addTask(payload)); // update store
   }
